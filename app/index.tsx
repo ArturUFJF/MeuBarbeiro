6255fadfa462dd728b691eb326174cffd801efb1
Artur Castro <arturcastro010@gmail.com>
2024-08-15 19:48:02 -0300

    Visualização da página de login finalizada

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import {View, Text, StatusBar, SafeAreaView, StyleSheet} from "react-native";
 import AppLoading from "expo-app-loading";
-import {useFonts, Raleway_900Black, Raleway_600SemiBold, Raleway_700Bold} from "@expo-google-fonts/raleway";
+import {useFonts, Raleway_600SemiBold, Raleway_700Bold, Raleway_900Black} from "@expo-google-fonts/raleway";
 import {SquadaOne_400Regular} from "@expo-google-fonts/squada-one";
 import {Oswald_300Light, Oswald_400Regular, Oswald_500Medium, Oswald_600SemiBold, Oswald_700Bold} from "@expo-google-fonts/oswald";
 import Navbar from "../components/navbar/navbar";
-import Home from "../components/home/home";
 import Login from "../components/login/login"
 
 export default function Index(){
@@ -13,6 +12,7 @@ export default function Index(){
     const [LoadFont] = useFonts({
         "RalewaySemiBold": Raleway_600SemiBold,
         "RalewayBold": Raleway_700Bold,
+        "RalewayBlack": Raleway_900Black,
         "SquadaOne": SquadaOne_400Regular,
         "OswaldLight": Oswald_300Light,
         "OswaldRegular": Oswald_400Regular,
--- components/login/login.tsx
+++ components/login/login.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {View, Text, StyleSheet, SafeAreaView, TextInput} from "react-native";
+import {View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity} from "react-native";
 
 export default function login(){
     return <>
@@ -13,6 +13,13 @@ export default function login(){
         <TextInput style={styles.field}/>
         <Text style={styles.fieldName}> Senha</Text>
         <TextInput style={styles.field}/>
+
+        <TouchableOpacity style={styles.button}>
+            {
+                // adicionar simulação de validação depois
+            }
+            <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
 
     </View>
     
@@ -30,7 +37,7 @@ const styles = StyleSheet.create({
     welcome: {
         paddingTop: 28,
         fontFamily: "SquadaOne",
-        fontSize: 36,
+        fontSize: 40,
     },
 
     loginView: {
@@ -47,6 +54,7 @@ const styles = StyleSheet.create({
     field: {
         alignSelf: "center",
         width: "100%",
+        padding: 8,
         height: 48,
         borderWidth: 2,
         borderColor: "#4E8098",
@@ -54,4 +62,21 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         marginBottom: 24,
     },
+
+    button: {
+        width: "100%",
+        marginTop: 28,
+        height: 56,
+        backgroundColor: "#A31621",
+        borderRadius: 16,
+        justifyContent: "center",
+    },
+
+    buttonText: {
+        fontFamily: "RalewayBlack",
+        textAlign: "center",
+        color: "#FCF7F8",
+        fontSize: 26,
+    }
+
 })
